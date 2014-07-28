11e390563f625a952b9b18fb407a882bd911565c
lcs-pc-inno <598420668@163.com>
2014-07-28 15:52:38 +0800

    添加注释

--- ID3.js
+++ ID3.js
@@ -1,9 +1,20 @@
 (function(win){
 	"use strict";
 
-	var ID3 = function( file , chartSet ){
+	var ID3 = function( file ){
 		var $this = this;
 		
+		/**
+		 * 读取文件
+		 * @param  int   start 起始字节下标
+		 * @param  int   end   结束字节下标（不包含end）
+		 * @param  Function cb 回调，返回结果
+		 * @param  string   type  [可选]，读取类型或字符编码，可为：
+		 *                        		1、dataurl:返回一个base64数据
+		 *                        		2、byte：返回一个Uint8Array数据
+		 *                        		3、相应字符集，如 'urf-8'
+		 * @return void
+		 */
 		var read = this.read = function(start , end , cb , type){
 			start = Math.floor(start) || 0;
 			end = Math.floor(end) || 0;
@@ -29,6 +40,12 @@
 			}
 		};
 
+		/**
+		 * 将一个Uint8Array转为一个整数
+		 * @param  Uint8Array data 数据
+		 * @param  int base 进制
+		 * @return int
+		 */
 		var coverToInt = function(data , base){
 			base = base || 128;
 			var total = 0;
@@ -38,16 +55,30 @@
 			return total;
 		};
 
+		/**
+		 * 将一个Uint8Array转为一个String
+		 * @param  Uint8Array   data 数据
+		 * @param  Function cb   [可选],可用来中断转换
+		 * @return String
+		 */
 		var coverToStr = function( data , cb ){
 			var str = "";
 			for( var i = 0 ; i < data.length ; i++ ){
 				var code = cb ? cb.call( data[i] , data[i] , i ) : data[i];
 				if( code === false )return str;
-				str +=  String.fromCharCode(code) ;
+				str +=  String.fromCharCode(code || data[i]) ;
 			}
 			return str;
 		};
 
+		/**
+		 * 读取图片信息
+		 * @param  int   start 起始字节下标
+		 * @param  int   end   结束字节下标（不包含end）
+		 * @param  Uint8Array   byteData 为图片标签的Uint8Array数据
+		 * @param  Function cb  读取成功后调用，传入一入String和一个Blob
+		 * @return void
+		 */
 		var readPic = function(index,end,byteData,cb){
 			var offset = 1;
 			var format = coverToStr(byteData.subarray(offset), function( code , index ){
@@ -82,6 +113,11 @@
 			//},"dataurl");
 		};
 
+		/**
+		 * 获取字符集编码
+		 * @param  int type 
+		 * @return 
+		 */
 		var getCharSet = (function(){
 			var data = {
 				"0":"iso-8859-1",
@@ -94,12 +130,23 @@
 			}
 		})();
 
+		/**
+		 * 获取总大小
+		 * @param  Function cb 回调
+		 * @return void
+		 */
 		this.getTotalSize = function(cb){
 			read(6,10,function(data){
 				cb.call( this , coverToInt(data,128) );
 			},"byte");
 		};
 
+		/**
+		 * 读取一个标签
+		 * @param  int   index 标签起始下标
+		 * @param  Function cb 读取成功后回调，传入标签的相关信息
+		 * @return 
+		 */
 		this.readTag = function(index,cb){
 			read( index , index + 10 , function(head){
 				var end = index + 10 + coverToInt( head.subarray(4,8) , 256 );
@@ -120,9 +167,13 @@
 			} , "byte");
 		};
 
+		/**
+		 * 获取ID3相关信息
+		 * @param  Function cb 	读取成功后回调，传入标签的相关信息
+		 * @return void
+		 */
 		this.getData = function( cb ){
 			read(0,10,function(id3){
-
 				if( !/id3/i.test(coverToStr( id3.subarray(0,3)) ) || id3[3] < 3  )return cb(false);
 				this.getTotalSize(function(totalSize){
 					var end = totalSize +0;
